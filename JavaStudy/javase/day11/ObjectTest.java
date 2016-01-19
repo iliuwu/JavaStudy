e52e56edfde37f68ea4a164f223bb1a740b6091e
iliuwu <iliuwu@outlook.com>
2016-01-19 16:09:51 +0800

    1.19commit

---
+++ JavaStudy/javase/day11/ObjectTest.java
@@ -0,0 +1,13 @@
+package day11;
+
+public class ObjectTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string = "Hello,Java";
+		String string2 = "Hello,Java";
+		System.out.println(string==string2);
+	
+	}
+
+}
